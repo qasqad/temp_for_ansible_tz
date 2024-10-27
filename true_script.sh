7580e4f706ab85d665cf3bc58917beab5fc7744a
evgenii.b <en@noemail.com>
2024-10-28 00:07:27 +0500

    fix

--- true_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mv /app/vpnserver.sh /opt/vpnserver.sh && chmod 755 /opt/vpnserver.sh
-mv /app/vpnserver /usr/local/
-chmod 600 /usr/local/vpnserver/
-chmod 700 /usr/local/vpnserver/vpncmd
-chmod 700 /usr/local/vpnserver/vpnserver
